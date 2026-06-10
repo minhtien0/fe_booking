df72329d9a5523fc36a30518d868c64e31dfecda
minhtien0 <tiennguyen14700@gmail.com>
2026-06-10 14:13:24 +0700

    [fix] Bug run build

--- src/sections/blog/BlogPage.tsx
+++ src/sections/blog/BlogPage.tsx
@@ -63,7 +63,6 @@ export default function BlogPage({
                     <Pagination
                       currentPage={currentPage}
                       totalPages={totalPages}
-                      basePath="/blog"
                     />
                   )}
                 </>
